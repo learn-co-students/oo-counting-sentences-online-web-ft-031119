c62df1c9d52019dd3f04d5996c9029b8b5d2b5af
Annie Phan <anniephan.atp@gmail.com>
2019-03-20 05:42:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    true ? self.end_with?(".") : false
   end
 
   def question?
-
+    true ? self.end_with?("?") : false
   end
 
   def exclamation?
-
+    true ? self.end_with?("!") : false
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).delete_if {|element| element.length < 1}.count
   end
+
 end
